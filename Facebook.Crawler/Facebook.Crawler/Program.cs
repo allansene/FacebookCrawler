3133f07cc6b9c3b1b6a68bfd0923171a593b49e3
allansene <allsene@dcc.ufmg.br>
2016-05-27 17:08:23 -0300

    tentativa de pegar o token de acesso definitivo.

--- Facebook.Crawler/Facebook.Crawler/Program.cs
+++ Facebook.Crawler/Facebook.Crawler/Program.cs
@@ -19,10 +19,30 @@ namespace Facebook.Crawler
         {
             try
             {
-                string access_token = "EAACEdEose0cBAHnmJ4ZAKhoZA9kCwtX4pMK2qYW3Ngbno6kJeBmvbN6Mo3JnDOEZBN5KkkAnngQWXa25ZCAz0oD1hMFn1kg8kC3PmGXqRWljEnQeUeHNXK6mE5c1AuBurnAbN8716zPKQDvZAE5ARq6aZB2ilm8iqqX9Dew0huGdOMCj4CCmmo";
-
+                //string access_token = "EAACEdEose0cBAHnmJ4ZAKhoZA9kCwtX4pMK2qYW3Ngbno6kJeBmvbN6Mo3JnDOEZBN5KkkAnngQWXa25ZCAz0oD1hMFn1kg8kC3PmGXqRWljEnQeUeHNXK6mE5c1AuBurnAbN8716zPKQDvZAE5ARq6aZB2ilm8iqqX9Dew0huGdOMCj4CCmmo";
                 var _fb = new FacebookClient();
-                _fb.AccessToken = access_token;
+                FacebookOAuthResult oauthResult;
+
+                
+                dynamic parameters = new ExpandoObject();
+                parameters.client_id = "1596455934002644";
+                parameters.response_type = "token";
+                parameters.display = "page";
+
+
+                var extendedPermissions = "user_likes,user_friends";
+                parameters.scope = extendedPermissions;
+                Uri uri = _fb.GetLoginUrl(parameters);
+                //_fb.AccessToken = access_token;
+
+                var oauthValid = _fb.TryParseOAuthCallbackUrl(uri, out oauthResult);
+                parameters.client_secret = "589bde7fbe33850dd8846151d847520b";
+                parameters.code = oauthResult.Code;
+
+                dynamic result = _fb.Get("/oauth/access_token", parameters);
+                
+                _fb.AccessToken = result.access_token;
+
                 var retorno = _fb.Get("search?q=spotted+ufmg&type=page").ToString();
 
                 SearchPageResult spottedUFMGPages = JsonConvert.DeserializeObject<SearchPageResult>(retorno);
